d641775ca3b1d132e4e4f9ec12305fc1bb3f6e9b
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-12-25 23:02:49 +0100

    display genre images

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,5 @@
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
 	const { data } = useGenres();
@@ -6,7 +7,14 @@ const GenreList = () => {
 	return (
 		<ul>
 			{data.map((genre) => (
-				<li key={genre.id}>{genre.name}</li>
+				<li key={genre.id}>
+					<img
+						style={{ width: '40px', height: '40px', borderRadius: '5px' }}
+						src={getCroppedImageUrl(genre.image_background)}
+						alt={genre.image_background}
+					/>
+					<p>{genre.name}</p>
+				</li>
 			))}
 		</ul>
 	);
