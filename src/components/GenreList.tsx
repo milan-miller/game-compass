9140a982dc1b42868f9a136380768c93774591af
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-12-25 23:15:17 +0100

    add a loading spinner when fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,12 @@
+import { ScaleLoader } from 'react-spinners';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-	const { data } = useGenres();
+	const { data, isLoading, error } = useGenres();
+
+	if (error) return null;
+	if (isLoading) return <ScaleLoader color='grey' height={20} width={2} />;
 
 	return (
 		<ul>
