2f02d54ecd370fa4519efb7eb6a8d331ef319a50
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-10 00:43:42 +0100

    add: infinite querying

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { GameQuery } from '../App';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
@@ -9,27 +10,46 @@ interface Props {
 }
 
 const GamesGrid = ({ gameQuery }: Props) => {
-	const { data, error, isLoading } = useGames(gameQuery);
+	const {
+		data,
+		error,
+		isLoading,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useGames(gameQuery);
 
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
 	if (error) return <p>{error.message}</p>;
 
 	return (
-		<ul>
-			{isLoading &&
-				skeletons.map((skeleton) => (
-					<GameCardContainer key={skeleton}>
-						<GameCardSkeleton />
-					</GameCardContainer>
-				))}
-			{!isLoading &&
-				data?.results.map((game) => (
-					<GameCardContainer key={game.id}>
-						<GameCard game={game} />
-					</GameCardContainer>
-				))}
-		</ul>
+		<div>
+			<ul>
+				{isLoading &&
+					skeletons.map((skeleton) => (
+						<GameCardContainer key={skeleton}>
+							<GameCardSkeleton />
+						</GameCardContainer>
+					))}
+
+				{!isLoading &&
+					data?.pages.map((page, index) => (
+						<React.Fragment key={index}>
+							{page.results.map((game) => (
+								<GameCardContainer key={game.id}>
+									<GameCard game={game} />
+								</GameCardContainer>
+							))}
+						</React.Fragment>
+					))}
+			</ul>
+			{hasNextPage && (
+				<button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
+					{isFetchingNextPage ? 'Loading...' : 'Load More'}
+				</button>
+			)}
+		</div>
 	);
 };
 export default GamesGrid;
