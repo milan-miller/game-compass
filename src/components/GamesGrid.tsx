a16363ae73eb54006f62d91b724c65867a15e311
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-10 01:13:19 +0100

    add: infinite scrolling

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -4,6 +4,8 @@ import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { PacmanLoader } from 'react-spinners';
 
 interface Props {
 	gameQuery: GameQuery;
@@ -23,32 +25,43 @@ const GamesGrid = ({ gameQuery }: Props) => {
 
 	if (error) return <p>{error.message}</p>;
 
+	const fetchedGamesCount =
+		data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
 	return (
 		<div>
-			<ul>
-				{isLoading &&
-					skeletons.map((skeleton) => (
-						<GameCardContainer key={skeleton}>
-							<GameCardSkeleton />
-						</GameCardContainer>
-					))}
+			<InfiniteScroll
+				dataLength={fetchedGamesCount}
+				hasMore={!!hasNextPage}
+				next={() => fetchNextPage()}
+				loader={<PacmanLoader />}
+				style={{ overflow: 'hidden' }}
+			>
+				<ul>
+					{isLoading &&
+						skeletons.map((skeleton) => (
+							<GameCardContainer key={skeleton}>
+								<GameCardSkeleton />
+							</GameCardContainer>
+						))}
 
-				{!isLoading &&
-					data?.pages.map((page, index) => (
-						<React.Fragment key={index}>
-							{page.results.map((game) => (
-								<GameCardContainer key={game.id}>
-									<GameCard game={game} />
-								</GameCardContainer>
-							))}
-						</React.Fragment>
-					))}
-			</ul>
-			{hasNextPage && (
-				<button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
-					{isFetchingNextPage ? 'Loading...' : 'Load More'}
-				</button>
-			)}
+					{!isLoading &&
+						data?.pages.map((page, index) => (
+							<React.Fragment key={index}>
+								{page.results.map((game) => (
+									<GameCardContainer key={game.id}>
+										<GameCard game={game} />
+									</GameCardContainer>
+								))}
+							</React.Fragment>
+						))}
+				</ul>
+				{/* {hasNextPage && (
+					<button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
+						{isFetchingNextPage ? 'Loading...' : 'Load More'}
+					</button>
+				)} */}
+			</InfiniteScroll>
 		</div>
 	);
 };
