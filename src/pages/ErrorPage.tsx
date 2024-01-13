53ada042295bcbf0220c38d7503c1d4894d27b75
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-13 22:28:39 +0100

    add: invalid route error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+const ErrorPage = () => {
+	const error = useRouteError();
+
+	return (
+		<>
+			<NavBar />
+			<h1>Oops</h1>
+			<p>
+				{isRouteErrorResponse(error)
+					? 'This page does not exist'
+					: 'An unexpected error occurred.'}
+			</p>
+		</>
+	);
+};
+export default ErrorPage;
