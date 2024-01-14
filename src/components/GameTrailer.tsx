a2869db8108fc954ad23e10401d87b9c5a6e010e
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-14 15:53:24 +0100

    add: game trailer player

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,24 @@
+import useTrailers from '../hooks/useTrailers';
+
+interface Props {
+	gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+	const { data, error, isLoading } = useTrailers(gameId);
+
+	const firstTrailer = data?.results[0];
+
+	if (isLoading) return null;
+
+	if (error) throw error;
+
+	return firstTrailer ? (
+		<video
+			src={firstTrailer.data[480]}
+			poster={firstTrailer.preview}
+			controls
+		/>
+	) : null;
+};
+export default GameTrailer;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import useGame from '../hooks/useGame';
 import { RingLoader } from 'react-spinners';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
+import GameTrailer from '../components/GameTrailer';
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
 			<h1>{game.name}</h1>
 			<ExpandableText>{game.description_raw}</ExpandableText>
 			<GameAttributes game={game} />
+			<GameTrailer gameId={game.id} />
 		</>
 	);
 };
