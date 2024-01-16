bb76db451b085606596f1355ecd2b5061e403d79
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-16 20:38:17 +0100

    delete: emoji component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import { Game } from '../entities/Game';
 import getCroppedImageUrl from '../services/image-url';
 import CriticScore from './CriticScore';
-import Emoji from './Emoji';
 import PlatformIconList from './PlatformIconList';
 
 interface Props {
@@ -26,7 +25,6 @@ const GameCard = ({ game }: Props) => {
 			<Link to={`/games/${game.slug}`} className='gamecard__title'>
 				<h2>{game.name}</h2>
 			</Link>
-			<Emoji rating={game.rating_top} />
 		</div>
 	);
 };
