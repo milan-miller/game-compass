206f636ab57a90dbf47042be0310c010add73ede
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-16 19:23:47 +0100

    refactor: PlatformSelector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,43 +1,31 @@
-import { useState } from 'react';
 import usePlatforms from '../hooks/usePlatforms';
-import usePlatform from '../hooks/usePlatform';
 import useGameQueryStore from '../store';
 
 const PlatformSelector = () => {
 	const { data, error } = usePlatforms();
 	const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
-	const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
-	const selectedPlatform = usePlatform(selectedPlatformId);
-
-	const [isOpen, setIsOpen] = useState(false);
-
-	const toggleDropdown = () => {
-		setIsOpen(!isOpen);
-	};
 
 	if (error) return null;
 
 	return (
-		<div className='dropdown'>
-			<button onClick={toggleDropdown}>
-				{selectedPlatform?.name || 'Platforms'}
-			</button>
-			{isOpen && data && (
-				<div className='dropdown-content'>
-					{data?.results.map((platform) =>
-						platform !== selectedPlatform ? (
-							<p
-								key={platform.id}
-								onClick={() => {
-									setIsOpen(!isOpen), setSelectedPlatformId(platform.id);
-								}}
-							>
-								{platform.name}
-							</p>
-						) : null
-					)}
-				</div>
-			)}
+		<div className='platform-selector'>
+			<label htmlFor='platform'>Platform: </label>
+			<select
+				id='platform'
+				onChange={(e) => {
+					if (e.target.value === 'all') {
+						setSelectedPlatformId(undefined);
+					}
+					setSelectedPlatformId(parseInt(e.target.value));
+				}}
+			>
+				<option value='all'>{`All`}</option>
+				{data?.results.map((platform) => (
+					<option key={platform.id} value={platform.id}>
+						{platform.name}
+					</option>
+				))}
+			</select>
 		</div>
 	);
 };
